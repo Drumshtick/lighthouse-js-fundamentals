5c9327e30b597454593939f3c89f4c4f1db894c7
Erik Kodnar <ekodnar@outlook.com>
2021-09-28 17:04:33 +0000

    tested a return function

--- hello.js
+++ hello.js
@@ -6,3 +6,9 @@ sayHello("cookie");
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+const returnSayHello  = function (name) {
+  return "Hello, " + name;
+}
+const greeting = returnSayHello('John');
+
+console.log(greeting);
