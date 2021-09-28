13ba66bf9cef46a05ed1c5759f7a9cc637eda04e
Erik Kodnar <ekodnar@outlook.com>
2021-09-28 17:00:39 +0000

    Test of functions that take an argument then print that argument concatenated to a string

--- tinkering.js
+++ tinkering.js
@@ -1,10 +1,10 @@
-var num = 99;
-
-while (num > 0) {
-    if (num > 1) {
-        console.log(num + " bottles of juice on the wall! "+ num +" bottles of juice! Take one down, pass it around... "+ (num -1) +" bottles of juice on the wall!");
-    } else {
-        console.log("1 bottle of juice on the wall! 1 bottle of juice! Take one down, pass it around... 0 bottles of juice on the wall!");
-    }
-    num -= 1;
+function emotions(myString, myFunc) {
+    console.log("I am " + myString + ", " + myFunc(2));
 }
+emotions("happy", const funny = function(num) {
+    let string = "";
+    for(let i = 0; i <= num; i += 1) {
+        string += "haha";
+    }
+    return string += "!";
+});
